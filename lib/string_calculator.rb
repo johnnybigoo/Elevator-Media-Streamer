5de915842a83ea6274a20837ab74f7e4ed12bcda
johnnybigoo <jotalico@icloud.com>
2021-12-07 11:13:16 -0500

    RSpec demo tests

---
+++ lib/string_calculator.rb
@@ -0,0 +1,16 @@
+class StringCalculator
+  
+  def self.add(input)
+    # input.size
+    # input.tr('^0-9', '').size
+    string_parsed = input.tr('^0-9', '')
+    string_parsed_size = string_parsed.size
+
+    if string_parsed.size == 0
+      string_parsed = nil
+      string_parsed_size = 0
+    end
+
+    {size: string_parsed_size,  value: string_parsed}
+  end
+end
